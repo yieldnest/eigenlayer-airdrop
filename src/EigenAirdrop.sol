40ebdcafa053a1ba4c113392ec5b610ebb819da2
MrDeadCe11 <mister.deadce11@gmail.com>
2024-09-21 14:46:10 -0500

    remove unwanted files

--- src/EigenAirdrop.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.25;
-
-contract EigenAirdrop {
-    mapping(address userAddress => uint256 points)public userPoints;
-    function id(uint256 value) external pure returns (uint256) {
-        return value;
-    }
-}
