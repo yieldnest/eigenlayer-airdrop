ed927edbd14b8ea33e8dbf2052fa1e774e6c2b96
dan13ram <dan13ram@gmail.com>
2024-09-24 15:48:07 +0530

    added SigUtils

--- src/EigenAirdrop.sol
+++ src/EigenAirdrop.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.8.25;
 
 import { OwnableUpgradeable } from "@openzeppelin-upgradeable-v5.0.2/access/OwnableUpgradeable.sol";
@@ -15,13 +15,30 @@ import {
     IStrategyManager
 } from "eigenlayer-contracts/interfaces/IStrategyManager.sol";
 
-struct UserPoints {
+struct UserAmount {
     address user;
-    uint256 points;
+    uint256 amount;
 }
 
+/**
+ * @title IEigenAirdrop
+ * @dev Interface for EigenAirdrop contract with methods to claim and restake tokens.
+ */
 interface IEigenAirdrop {
+    /**
+     * @notice Claim tokens from the airdrop.
+     * @param _amountToClaim Amount of tokens to claim.
+     */
     function claim(uint256 _amountToClaim) external;
+
+    /**
+     * @notice Claim tokens from the airdrop using a permit for gasless approval.
+     * @param _amountToClaim Amount of tokens to claim.
+     * @param _deadline The deadline by which the permit must be executed.
+     * @param _v V parameter of the ECDSA signature.
+     * @param _r R parameter of the ECDSA signature.
+     * @param _s S parameter of the ECDSA signature.
+     */
     function claimWithPermit(
         uint256 _amountToClaim,
         uint256 _deadline,
@@ -30,6 +47,13 @@ interface IEigenAirdrop {
         bytes32 _s
     )
         external;
+
+    /**
+     * @notice Claim tokens from the airdrop and restake them using a signature.
+     * @param _amountToClaim Amount of tokens to claim.
+     * @param expiry The expiry time of the signature.
+     * @param signature The user's signature authorizing the restaking.
+     */
     function claimAndRestakeWithSignature(
         uint256 _amountToClaim,
         uint256 expiry,
@@ -38,41 +62,83 @@ interface IEigenAirdrop {
         external;
 }
 
+/**
+ * @title EigenAirdrop
+ * @dev A contract that manages token airdrops and allows users to claim and restake tokens.
+ */
 contract EigenAirdrop is IEigenAirdrop, OwnableUpgradeable, PausableUpgradeable, ReentrancyGuardUpgradeable {
     using SafeERC20 for IERC20;
     using Math for uint256;
 
-    mapping(address userAddress => uint256 amount) public amounts;
-    uint256 public totalPoints;
+    /// @notice Stores the claimable amounts for each user.
+    mapping(address user => uint256 amount) public amounts;
+
+    /// @notice The total amount of tokens available for the airdrop.
+    uint256 public totalAmount;
+
+    /// @notice Address of the safe that holds the tokens.
     address public safe;
+
+    /// @notice The token being airdropped.
     IERC20 public token;
+
+    /// @notice The strategy where tokens can be restaked.
     IStrategy public strategy;
+
+    /// @notice The strategy manager responsible for managing the strategy.
     IStrategyManager public strategyManager;
 
-    event AirdropClaimed(address user, uint256 amount);
-    event AirdropClaimedAndRestaked(address user, uint256 amount);
+    /// @notice Emitted when a user claims tokens from the airdrop.
+    /// @param user The address of the user claiming tokens.
+    /// @param amount The amount of tokens claimed.
+    event Claimed(address user, uint256 amount);
 
+    /// @notice Emitted when a user claims and restakes tokens.
+    /// @param user The address of the user.
+    /// @param amount The amount of tokens restaked.
+    event ClaimedAndRestaked(address user, uint256 amount);
+
+    /// @notice Error thrown when there is no airdrop available.
     error NoAirdrop();
+    error InvalidAirdrop();
 
-    modifier canClaim(uint256 _amountToClaim) {
+    /**
+     * @dev Modifier to check if the user can claim the specified amount.
+     * Reverts if the amount is zero or exceeds the claimable balance.
+     * @param _amountToClaim The amount the user is trying to claim.
+     */
+    modifier whenAvailable(uint256 _amountToClaim) {
         if (_amountToClaim == 0 || _amountToClaim > amounts[msg.sender]) {
             revert NoAirdrop();
         }
         _;
         amounts[msg.sender] -= _amountToClaim;
+        totalAmount -= _amountToClaim;
     }
 
+    /**
+     * @dev Disables initializers to prevent contract from being reinitialized.
+     */
     constructor() {
         _disableInitializers();
     }
 
+    /**
+     * @notice Initializes the airdrop contract with the provided parameters.
+     * @param _owner The address of the owner.
+     * @param _safe The address of the safe holding the tokens.
+     * @param _token The address of the token being airdropped.
+     * @param _strategy The address of the strategy for restaking.
+     * @param _strategyManager The address of the strategy manager.
+     * @param _userAmounts An array of user amount for token distribution.
+     */
     function initialize(
         address _owner,
         address _safe,
         address _token,
         address _strategy,
         address _strategyManager,
-        UserPoints[] memory _userPoints
+        UserAmount[] memory _userAmounts
     )
         public
         initializer
@@ -84,41 +150,65 @@ contract EigenAirdrop is IEigenAirdrop, OwnableUpgradeable, PausableUpgradeable,
         token = IERC20(_token);
         strategy = IStrategy(_strategy);
         strategyManager = IStrategyManager(_strategyManager);
-        _updateUserPoints(_userPoints);
+        _updateUserAmounts(_userAmounts);
     }
 
+    /**
+     * @notice Pauses the contract, preventing claims.
+     * Can only be called by the owner.
+     */
     function pause() external onlyOwner whenNotPaused {
         _pause();
     }
 
+    /**
+     * @notice Unpauses the contract, allowing claims.
+     * Can only be called by the owner.
+     */
     function unpause() external onlyOwner whenPaused {
         _unpause();
     }
 
-    function updateUserPoints(UserPoints[] memory _userPoints) external onlyOwner whenPaused {
-        _updateUserPoints(_userPoints);
+    /**
+     * @notice Updates user amount for the airdrop. Only callable by the owner when the contract is paused.
+     * @param _userAmounts An array of updated user amount.
+     */
+    function updateUserAmounts(UserAmount[] memory _userAmounts) external onlyOwner whenPaused {
+        _updateUserAmounts(_userAmounts);
     }
 
-    function _updateUserPoints(UserPoints[] memory _userPoints) internal {
-        uint256 initialMultiSigBalance = token.balanceOf(safe);
-        if (initialMultiSigBalance == 0) {
+    /**
+     * @dev Internal function to update user amount and recalculate claimable amounts.
+     * @param _userAmounts An array of updated user amount.
+     */
+    function _updateUserAmounts(UserAmount[] memory _userAmounts) internal {
+        for (uint256 i; i < _userAmounts.length; i++) {
+            totalAmount += _userAmounts[i].amount;
+            amounts[_userAmounts[i].user] += _userAmounts[i].amount;
+        }
+
+        uint256 safeBalance = token.balanceOf(safe);
+        if (safeBalance == 0 || totalAmount > safeBalance) {
             revert NoAirdrop();
         }
-
-        totalPoints = 0;
-        for (uint256 i; i < _userPoints.length; i++) {
-            totalPoints += _userPoints[i].points;
-        }
-
-        for (uint256 i; i < _userPoints.length; i++) {
-            amounts[_userPoints[i].user] = _userPoints[i].points.mulDiv(initialMultiSigBalance, totalPoints);
-        }
     }
 
+    /**
+     * @notice Claims the specified amount of tokens from the airdrop.
+     * @param _amountToClaim The amount of tokens to claim.
+     */
     function claim(uint256 _amountToClaim) external virtual override nonReentrant whenNotPaused {
         _claim(_amountToClaim);
     }
 
+    /**
+     * @notice Claims the specified amount of tokens from the airdrop using a permit for gasless approval.
+     * @param _amountToClaim The amount of tokens to claim.
+     * @param _deadline The deadline by which the permit must be executed.
+     * @param _v V parameter of the ECDSA signature.
+     * @param _r R parameter of the ECDSA signature.
+     * @param _s S parameter of the ECDSA signature.
+     */
     function claimWithPermit(
         uint256 _amountToClaim,
         uint256 _deadline,
@@ -136,11 +226,21 @@ contract EigenAirdrop is IEigenAirdrop, OwnableUpgradeable, PausableUpgradeable,
         _claim(_amountToClaim);
     }
 
-    function _claim(uint256 _amountToClaim) internal canClaim(_amountToClaim) {
+    /**
+     * @dev Internal function to handle the token claim process.
+     * @param _amountToClaim The amount of tokens to claim.
+     */
+    function _claim(uint256 _amountToClaim) internal whenAvailable(_amountToClaim) {
         token.safeTransferFrom(safe, msg.sender, _amountToClaim);
-        emit AirdropClaimed(msg.sender, _amountToClaim);
+        emit Claimed(msg.sender, _amountToClaim);
     }
 
+    /**
+     * @notice Claims and restakes the specified amount of tokens using a signature.
+     * @param _amountToClaim The amount of tokens to claim.
+     * @param _expiry The expiry time of the signature.
+     * @param signature The user's signature authorizing the restaking.
+     */
     function claimAndRestakeWithSignature(
         uint256 _amountToClaim,
         uint256 _expiry,
@@ -155,18 +255,24 @@ contract EigenAirdrop is IEigenAirdrop, OwnableUpgradeable, PausableUpgradeable,
         _claimAndRestakeWithSignature(_amountToClaim, _expiry, signature);
     }
 
+    /**
+     * @dev Internal function to handle the claim and restaking process.
+     * @param _amountToClaim The amount of tokens to claim.
+     * @param _expiry The expiry time of the signature.
+     * @param signature The user's signature authorizing the restaking.
+     */
     function _claimAndRestakeWithSignature(
         uint256 _amountToClaim,
         uint256 _expiry,
         bytes calldata signature
     )
         internal
-        canClaim(_amountToClaim)
+        whenAvailable(_amountToClaim)
     {
         token.safeTransferFrom(safe, address(this), _amountToClaim);
         strategyManager.depositIntoStrategyWithSignature(
             strategy, IStrategyToken(address(token)), _amountToClaim, msg.sender, _expiry, signature
         );
-        emit AirdropClaimedAndRestaked(msg.sender, _amountToClaim);
+        emit ClaimedAndRestaked(msg.sender, _amountToClaim);
     }
 }
