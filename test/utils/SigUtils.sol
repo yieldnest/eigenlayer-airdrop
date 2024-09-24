de28a14b4979d36f89da661cfe5b0c6d89adaaf5
dan13ram <dan13ram@gmail.com>
2024-09-24 16:45:12 +0530

    claim and stake works

--- test/utils/SigUtils.sol
+++ test/utils/SigUtils.sol
@@ -5,18 +5,6 @@ interface IStrategyManager {
     function domainSeparator() external view returns (bytes32);
 }
 
-interface IPermitToken {
-    function DOMAIN_SEPARATOR() external view returns (bytes32);
-}
-
-struct Permit {
-    address owner;
-    address spender;
-    uint256 value;
-    uint256 nonce;
-    uint256 deadline;
-}
-
 struct Deposit {
     address staker;
     address strategy;
@@ -27,88 +15,29 @@ struct Deposit {
 }
 
 library SigUtils {
-
     bytes32 public constant DOMAIN_TYPEHASH =
         keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract)");
 
-    bytes32 public constant DEPOSIT_TYPEHASH =
-        keccak256("Deposit(address staker,address strategy,address token,uint256 amount,uint256 nonce,uint256 expiry)");
+    bytes32 public constant DEPOSIT_TYPEHASH = keccak256(
+        "Deposit(address staker,address strategy,address token,uint256 amount,uint256 nonce,uint256 expiry)"
+    );
 
-    bytes32 public constant PERMIT_TYPEHASH =
-        keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
-
-    function getPermitStructHash(Permit memory _permit)
-        internal
-        pure
-        returns (bytes32)
-    {
-        return
-            keccak256(
-                abi.encode(
-                    PERMIT_TYPEHASH,
-                    _permit.owner,
-                    _permit.spender,
-                    _permit.value,
-                    _permit.nonce,
-                    _permit.deadline
-                )
-            );
+    function getDepositStructHash(Deposit memory _deposit) internal pure returns (bytes32) {
+        return keccak256(
+            abi.encode(
+                DEPOSIT_TYPEHASH,
+                _deposit.staker,
+                _deposit.strategy,
+                _deposit.token,
+                _deposit.amount,
+                _deposit.nonce,
+                _deposit.expiry
+            )
+        );
     }
 
-    function getDepositStructHash(Deposit memory _deposit)
-        internal
-        pure
-        returns (bytes32)
-    {
-        return
-            keccak256(
-                abi.encode(
-                    DEPOSIT_TYPEHASH,
-                    _deposit.staker,
-                    _deposit.strategy,
-                    _deposit.token,
-                    _deposit.amount,
-                    _deposit.nonce,
-                    _deposit.expiry
-                )
-            );
-    }
-
-    function getPermitDigest(
-        address token,
-        Permit memory _permit)
-        public
-        view
-        returns (bytes32)
-    {
-
-        bytes32 domainSeparator = IPermitToken(token).DOMAIN_SEPARATOR();
-        return
-            keccak256(
-                abi.encodePacked(
-                    "\x19\x01",
-                    domainSeparator,
-                    getPermitStructHash(_permit)
-                )
-            );
-    }
-
-    function getDepositDigest(
-        address strategyManager,
-        Deposit memory _deposit)
-        public
-        view
-        returns (bytes32)
-    {
+    function getDepositDigest(address strategyManager, Deposit memory _deposit) public view returns (bytes32) {
         bytes32 domainSeparator = IStrategyManager(strategyManager).domainSeparator();
-        return
-            keccak256(
-                abi.encodePacked(
-                    "\x19\x01",
-                    domainSeparator,
-                    getDepositStructHash(_deposit)
-                )
-            );
+        return keccak256(abi.encodePacked("\x19\x01", domainSeparator, getDepositStructHash(_deposit)));
     }
 }
-
